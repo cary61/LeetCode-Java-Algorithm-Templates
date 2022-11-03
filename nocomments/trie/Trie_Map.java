630031a46c56c239a7425692377fe58df37ea9f8
Cary Zheng <caryzheng61@gmail.com>
2022-11-04 03:19:51 +0800

    Add Trie_Map.java of no comment version, a trie implementation of hash map so it can deal with not only lowercase string

---
+++ nocomments/trie/Trie_Map.java
@@ -0,0 +1,56 @@
+package nocomments.trie;
+
+import java.util.*;
+
+class Trie_Map {
+
+    class Node {
+        Map<Character,Node> next = new HashMap<Character,Node>();
+        boolean end;
+    }
+
+    Node root;
+
+    Trie_Map() {
+        root = new Node();
+    }
+
+    void insert(String word) {
+        int len = word.length();
+        Node p = root;
+        for (int i = 0; i < len; ++i) {
+            char ch = word.charAt(i);
+            if (p.next.get(ch) == null) {
+                p.next.put(ch, new Node());
+            }
+            p = p.next.get(ch);
+        }
+        p.end = true;
+    }
+
+    boolean containsPrefix(String word) {
+        int len = word.length();
+        Node p = root;
+        for (int i = 0; i < len; ++i) {
+            char ch = word.charAt(i);
+            if (p.next.get(ch) == null) {
+                return false;
+            }
+            p = p.next.get(ch);
+        }
+        return true;
+    }
+
+    boolean containsWord(String word) {
+        int len = word.length();
+        Node p = root;
+        for (int i = 0; i < len; ++i) {
+            char ch = word.charAt(i);
+            if (p.next.get(ch) == null) {
+                return false;
+            }
+            p = p.next.get(ch);
+        }
+        return p.end;
+    }
+}
